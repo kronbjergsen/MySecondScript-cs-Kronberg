51408c655098566aa2a104d7b746684451d174a3
Kronberger S <kronberger@Kronbergers-MacBook-Air.local>
2019-11-04 19:38:57 +0100

    Added debug log

--- Assets/MyGame/Scripts/HelloWorldHome.cs
+++ Assets/MyGame/Scripts/HelloWorldHome.cs
@@ -7,7 +7,7 @@ public class HelloWorldHome : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hello World at Home");
     }
 
     // Update is called once per frame
